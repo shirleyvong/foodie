8c7847c19f165de4eb985e5e90f97c8d66a8cf06
svong <svong@sfu.ca>
2019-05-18 15:16:06 -0700

    Add validation for form fields

---
+++ public/js/login.js
@@ -0,0 +1,24 @@
+// Validation for login form
+const username = document.getElementById("login-username");
+const password = document.getElementById("login-password");
+const loginForm = document.getElementById("login-form");
+const msg = document.getElementsByClassName("missing-input-msg")[0];
+
+function validate() {
+    username.classList.remove("missing-input");
+    password.classList.remove("missing-input");
+
+    if (username.value.length == 0) {
+        msg.innerHTML = "Username is required.";
+        username.classList.add("missing-input"); 
+        return;
+    }
+
+    if (password.value.length == 0) {
+        msg.innerHTML = "Password is required.";
+        password.classList.add("missing-input"); 
+        return;
+    }
+
+    loginForm.submit();
+}
